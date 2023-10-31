8047bf2ec4575cc961cc78c6a8de3d7718856429
Daniel Pett <dejp3@cam.ac.uk>
2023-10-31 15:24:18 +0000

    Add more details to readme

--- src/components/structure/bgImage.js
+++ src/components/structure/bgImage.js
@@ -4,37 +4,37 @@ import styled from "styled-components"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 const BackgroundImage = styled(GatsbyImage)`
-  height: 65vh;
-  width: 100%;
-  background-size: cover;
-    background-position: top center;
-  background-attachment: fixed;
+height: 65vh;
+width: 100%;
+background-size: cover;
+background-position: top center;
+background-attachment: fixed;
 `
 export default function Hero() {
-    const imagedata = useStaticQuery(graphql`
-        query BackgroundImageQuery {
-            file(relativePath: { eq: "Stonehenge.jpg" }) {
-                childImageSharp {
-                    gatsbyImageData(
-                        layout: FULL_WIDTH
-                        quality: 80
-                        backgroundColor: "black"
-                        jpgOptions: {quality: 80}
-                        transformOptions: {duotone: {
-                            highlight: "#ffffff"
-                            shadow: "#222222"
-                            opacity: 80
-                        }}
-                    )
-                }
-            }
+  const imagedata = useStaticQuery(graphql`
+    query BackgroundImageQuery {
+      file(relativePath: { eq: "Stonehenge.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(
+            layout: FULL_WIDTH
+            quality: 80
+            backgroundColor: "black"
+            jpgOptions: {quality: 80}
+            transformOptions: {duotone: {
+              highlight: "#ffffff"
+              shadow: "#222222"
+              opacity: 80
+            }}
+          )
         }
+      }
+    }
     `)
     return(
-            <BackgroundImage
-                className="background-image"
-                image={imagedata.file.childImageSharp.gatsbyImageData}
-                alt={'Stonehenge seen from the Heel stone'}
-            />
+      <BackgroundImage
+      className="background-image"
+      image={imagedata.file.childImageSharp.gatsbyImageData}
+      alt={'Stonehenge seen from the Heel stone'}
+      />
     )
-}
+  }
